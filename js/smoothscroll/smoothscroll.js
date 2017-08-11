9f29e30122084ab1e1483686dd609e5b5a71eb28
Nalopka <Nalopka@gmail.com>
2017-08-11 14:19:36 +0700

    add project

---
+++ js/smoothscroll/smoothscroll.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+$("a").on('click', function(event) {
+
+if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+});
